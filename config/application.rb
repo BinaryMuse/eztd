f7a411577874af7f6a697659469d4c62e29d5d35
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-29 11:41:27 -0700

    Add views

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,7 @@ module Todos
     # JavaScript files you want as :defaults (application.js is always included).
     config.action_view.javascript_expansions[:defaults] = %w(
       https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js
+      https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js
       rails
     )
 
---
+++ public/javascripts/application.js
@@ -0,0 +1,21 @@
+$(function() {
+  $("#new_task #task_text").focus();
+
+  $(".toggler input").live('change', function() {
+    $(this).parent('form').submit();
+  });
+
+  $("#new_task input[type=image]").hover(function() {
+    $(this).attr("src", "/images/add_button_reverse.png");
+  }, function() {
+    $(this).attr("src", "/images/add_button.png");
+  });
+
+  $(".task").live({mouseenter: function() {
+    $(this).children('.remover_placeholder').css('display', 'none');
+    $(this).children('.remover').css('display', 'inline-block');
+  }, mouseleave: function() {
+    $(this).children('.remover_placeholder').css('display', 'inline-block');
+    $(this).children('.remover').css('display', 'none');
+  }});
+});
