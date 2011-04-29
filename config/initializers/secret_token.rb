3b18fc196702d64a394792ae099e81d350b67ed9
Brandon Tilley <binarymuse@binarymuse.net>
2011-04-28 22:08:28 -0700

    Initial commit ('rails new todos -J -T')

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = '8b87cbbee18c2cd0cbde90e59b0736c799c3648f7663a1db10975e7e519ac700633649eb5530b28b59aeb71cb35fedd172c30c6011c8fa594f53c2b334071239'
